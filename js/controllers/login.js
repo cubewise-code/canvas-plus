57d5e1f3550f9c96aab8bc2966b28348e80045c1
iliashapiro <ishapiro@cubewise.com>
2018-04-26 13:56:52 +1000

    Log in page username to null

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -1,4 +1,4 @@
 
 app.controller('LoginCtrl', ['$scope', '$rootScope', '$state', '$stateParams', '$timeout', '$http', '$localStorage', '$base64', function($scope, $rootScope, $state, $stateParams, $timeout, $http, $localStorage, $base64) {
-		
+		$root.user.FriendlyName = undefined;
 }]);
