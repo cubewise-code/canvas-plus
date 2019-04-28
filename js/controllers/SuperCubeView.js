ff18e72a87a0911ab64cce09e0eeb38d3222b61b
iliashapiro <ishapiro@cubewise.com>
2019-04-29 05:44:40 +1000

    select error has been handled if the chart is in change state

--- js/controllers/SuperCubeView.js
+++ js/controllers/SuperCubeView.js
@@ -699,25 +699,28 @@
           return val;
         } 
           scope.highlightPoints = function(chart){
-            scope.svg.selectAll("dot")    
-            .data(data)         
-        .enter().append("circle")                               
-            .attr("r", 5)       
-            .attr("cx", function(d) { return x(d.date); })       
-            .attr("cy", function(d) { return y(d.close); })     
-            .on("mouseover", function(d) {      
-                div.transition()        
-                    .duration(200)      
-                    .style("opacity", .9);      
-                div .html(formatTime(d.date) + "<br/>"  + d.close)  
-                    .style("left", (d3.event.pageX) + "px")     
-                    .style("top", (d3.event.pageY - 28) + "px");    
-                })                  
-            .on("mouseout", function(d) {       
-                div.transition()        
-                    .duration(500)      
-                    .style("opacity", 0);   
-            });
+            if(scope.svg != 'undefined' && scope.svg != null){
+              scope.svg.selectAll("dot")    
+              .data(data)         
+          .enter().append("circle")                               
+              .attr("r", 5)       
+              .attr("cx", function(d) { return x(d.date); })       
+              .attr("cy", function(d) { return y(d.close); })     
+              .on("mouseover", function(d) {      
+                  div.transition()        
+                      .duration(200)      
+                      .style("opacity", .9);      
+                  div .html(formatTime(d.date) + "<br/>"  + d.close)  
+                      .style("left", (d3.event.pageX) + "px")     
+                      .style("top", (d3.event.pageY - 28) + "px");    
+                  })                  
+              .on("mouseout", function(d) {       
+                  div.transition()        
+                      .duration(500)      
+                      .style("opacity", 0);   
+              });
+            }
+            
           }
   
          
