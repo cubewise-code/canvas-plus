97c0c04bd989c23683649a015eb3603ac14399d1
iliashapiro <ishapiro@cubewise.com>
2019-05-07 13:05:35 +1000

    drag and drop finished tracked by arrays

--- js/controllers/supercubeviewmdxcustompage.js
+++ js/controllers/supercubeviewmdxcustompage.js
@@ -53,6 +53,7 @@ function($scope, $rootScope, $log, $tm1Ui, $transitions,$location, $timeout, glo
         if($scope.mdxString === $scope.focusedMdxString){
             $scope.changedOption = false; 
         }else{
+            
             $scope.changedOption = true; 
         }
       //  console.log($scope.changedOption, "FOCUS OUT OF TEXTFIELD");
@@ -81,6 +82,10 @@ function($scope, $rootScope, $log, $tm1Ui, $transitions,$location, $timeout, glo
       
     
     }
+    $scope.rowDimensionArray = [];
+    $scope.columnDimensionArray = [];
+    $scope.tilesDimensionArray = [];
+    $scope.filtersDimensionArray = [];
     $scope.allowDrop= function (ev) {
         ev.preventDefault();
     };
@@ -92,21 +97,84 @@ function($scope, $rootScope, $log, $tm1Ui, $transitions,$location, $timeout, glo
   $scope.drop=  function (ev) {
         ev.preventDefault();
         var data = ev.dataTransfer.getData("Text");
+        console.log("Drop : Dimension",data, "on ", ev.target.getAttribute('id'))
+        if(($scope.rowDimensionArray).indexOf(data) === -1){
+             
+        }else{
+            $scope.rowDimensionArray.splice(($scope.rowDimensionArray).indexOf(data), 1);
+        }
+        if(($scope.filtersDimensionArray).indexOf(data) === -1){
+             
+        }else{
+            $scope.filtersDimensionArray.splice(($scope.filtersDimensionArray).indexOf(data), 1);
+        }
+         
+        if(($scope.columnDimensionArray).indexOf(data) === -1){
+            
+        }else{
+            $scope.columnDimensionArray.splice(($scope.columnDimensionArray).indexOf(data), 1);
+        }
+        if(($scope.tilesDimensionArray).indexOf(data) === -1){
+          
+        }else{
+            $scope.tilesDimensionArray.splice(($scope.tilesDimensionArray).indexOf(data), 1);
+        }
+        if(ev.target.getAttribute('id') === 'row'){
+            if(($scope.rowDimensionArray).indexOf(data) === -1){
+                $scope.rowDimensionArray.push(data);
+            }else{
+                //$scope.rowDimensionArray.splice(($scope.rowDimensionArray).indexOf(data), 1);
+            }
+             
+        }else{
+            if(ev.target.getAttribute('id') === 'col'){
+               
+                if(($scope.columnDimensionArray).indexOf(data) === -1){
+                    $scope.columnDimensionArray.push(data);
+                }else{
+                    $scope.columnDimensionArray.splice(($scope.columnDimensionArray).indexOf(data), 1);
+                }
+            }else{
+                if(ev.target.getAttribute('id') === 'tiles'){
+                    
+                    if(($scope.tilesDimensionArray).indexOf(data) === -1){
+                        $scope.tilesDimensionArray.push(data);
+                    }else{
+                        $scope.tilesDimensionArray.splice(($scope.tilesDimensionArray).indexOf(data), 1);
+                    }
+                }else{
+                    if(ev.target.getAttribute('id') === 'filters'){
+                    
+                        if(($scope.filtersDimensionArray).indexOf(data) === -1){
+                            $scope.filtersDimensionArray.push(data);
+                        }else{
+                            $scope.filtersDimensionArray.splice(($scope.filtersDimensionArray).indexOf(data), 1);
+                        }
+                    }
+                }
+                
+            }  
+        }
         
-       
-            ev.target.appendChild(document.getElementById(data));
-     
-        
+        //ev.target.appendChild(document.getElementById(data));
+        window.dispatchEvent(new Event('resize')); 
+        console.log($scope.tilesDimensionArray, $scope.columnDimensionArray,  $scope.rowDimensionArray, $scope.filtersDimensionArray)
     };
   
   
     $scope.cubeDimensionalityArray = [];
     $scope.formatMdxStringCube = function(cube){ 
          $scope.mdxStringCube =  cube;
+         $rootScope.cubeName = cube;
          $tm1Ui.cubeDimensions($scope.tm1Instance, cube).then(function(result){
             if(!result.failed){ 
+                
                 console.log("cubeNameChanged Dimensions to load",  result);
-                $scope.cubeDimensionalityArray = result;
+                //$scope.cubeDimensionalityArray = result;
+                $scope.tilesDimensionArray = result;
+                $rootScope.mdxString =  "SELECT { } ON COLUMNS, { } ON ROWS FROM ["+$rootScope.cubeName+"] WHERE ()";
+                //$rootScope.mdxString = "SELECT {[Period].[Year], [Period].[Jan], [Period].[Feb], [Period].[Mar], [Period].[Apr], [Period].[May], [Period].[Jun], [Period].[Jul], [Period].[Aug], [Period].[Sep], [Period].[Oct], [Period].[Nov], [Period].[Dec]} ON COLUMNS, {TM1DRILLDOWNMEMBER( {[Account].[Net Income]}, ALL, RECURSIVE )} ON ROWS FROM ["+$rootScope.cubeName+"] WHERE ([Year].&[2018], [Region].&[England], [General Ledger Measure].&[Amount], [Currency].&[Local], [Version].&[Budget], [Department].&[Corporate])" 
+    
             }
          }
          )
