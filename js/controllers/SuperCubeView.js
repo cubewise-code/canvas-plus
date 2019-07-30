4ab174c619116a878f854258cab0e343d44cd7e4
iliashapiro <ishapiro@cubewise.com>
2019-07-30 22:08:18 +0200

    new orange color instead of red for upload cells

--- js/controllers/SuperCubeView.js
+++ js/controllers/SuperCubeView.js
@@ -894,7 +894,7 @@
                                          }
                                       }
                                       
-                                      if(scope.createRequest === true && ((vv+'').split('(').join('-')).split(')').join('') !=  scope.table.data()[currentRow].cells[colCount].value ){
+                                      if(scope.createRequest === true && (((vv+'').split('(').join('-')).split(')').join('')).split(',').join('') !=  scope.table.data()[currentRow].cells[colCount].value ){
                                         console.log(scope.table.data()[currentRow].cells[colCount].value, "@@@@");
                                         scope.table.data()[currentRow].cells[colCount].cellUpdate = true;
                                         scope.table.data()[currentRow].cells[colCount].cellUpdateVal = ((vv+'').split('(').join('-')).split(')').join('');
